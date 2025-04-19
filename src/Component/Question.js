b63a56bc470567f986f660b427573477c984d2a1
SMRITI BHADOURIYA <162131977+smritibhadouriya@users.noreply.github.com>
2025-04-19 15:42:12 +0530

    Update Question.js

--- src/Component/Question.js
+++ src/Component/Question.js
@@ -1,18 +1,16 @@
 import React from 'react';
 
 const Question = ({ question, onAnswer }) => {
-  const { description, options } = question;
+  const { question: description, options } = question;
 
   return (
     <div className="question">
-      <h2 dangerouslySetInnerHTML={{ __html: description }} />
+      <h2>{description}</h2>
       <div className="answers">
         {options.map((option, index) => (
-          <button
-            key={index}
-            onClick={() => onAnswer(option.is_correct)}
-            dangerouslySetInnerHTML={{ __html: option.description }}
-          />
+          <button key={index} onClick={() => onAnswer(option)}>
+            {option}
+          </button>
         ))}
       </div>
     </div>
