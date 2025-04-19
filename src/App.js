6ec872af5aded3f056550b8bb57aa6d6033eab35
SMRITI BHADOURIYA <162131977+smritibhadouriya@users.noreply.github.com>
2025-04-19 15:36:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import StartPage from './Component/StartPage';
 import QuizApp from './Component/QuizApp';
 import ResultsPage from './Component/ResultsPage'; // Import the ResultsPage component
 
@@ -8,8 +7,7 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<StartPage />} />
-        <Route path="/quiz" element={<QuizApp />} />
+        <Route path="/" element={<QuizApp />} />
         <Route path="/results" element={<ResultsPage />} /> {/* Add the route for ResultsPage */}
       </Routes>
     </Router>
